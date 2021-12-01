40d3820823b1ab9faf59b3f5dd153ac9b2e45156
YaddictedToCodeH <95265095+YaddictedToCodeH@users.noreply.github.com>
2021-12-01 14:47:21 +0900

    Delete webProject directory

--- webProject/WebContent/js/banner.js
+++
@@ -1,9 +0,0 @@
-$(".slideList").children("div:gt(0)").hide();
-var current = 0;
-
-setInterval(function() {
-   var next = (current + 1) % 3;
-   $(".slideList").find("div").eq(current).fadeOut();
-   $(".slideList").find("div").eq(next).fadeIn();
-   current = next;
-}, 6000);
